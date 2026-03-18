b731a3a859ac755b02701507e2aceac12148fafc
Roberdan <roberdan@fightthestroke.org>
2026-03-18 20:52:56 +0100

    feat(telemetry): T1-01 unified telemetry SDK

---
+++ evolution/telemetry/index.ts
@@ -0,0 +1,12 @@
+export { CollectorRegistry } from './collectors.js';
+export { METRIC_FAMILIES, metricFamilySignals } from './metric-families.js';
+export { TelemetrySdk } from './sdk.js';
+export type {
+  CollectorRegistration,
+  FamilyCountMap,
+  FamilyQuota,
+  FamilyQuotaMap,
+  TelemetrySdkOptions,
+  TelemetrySinkFn,
+  TelemetrySnapshot,
+} from './types.js';
