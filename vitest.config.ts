d90e66ae20f6ad2ce370686c1e36f1fe592e3da2
Roberdan <roberdan@fightthestroke.org>
2026-03-18 23:19:44 +0100

    test: exclude playwright e2e specs from vitest

---
+++ vitest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  test: {
+    exclude: [
+      '**/node_modules/**',
+      '**/dist/**',
+      'dashboard/tests/e2e/**',
+      'dashboard_web/tests/e2e/**',
+    ],
+  },
+};
